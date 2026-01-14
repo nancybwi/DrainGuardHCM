0612ab62f37ebbbbc7da7fc8d88c38447558f80f
nancybwi <phuongthaotrinh0111@gmail.com>
2026-01-14 11:05:37 +0700

    git push test

---
+++ DrainGuardHCM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DrainGuardHCM
+//
+//  Created by Thao Trinh Phuong on 14/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, DrainGuardHCM")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrainGuardHCM/DrainGuardHCMApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrainGuardHCMApp.swift
+//  DrainGuardHCM
+//
+//  Created by Thao Trinh Phuong on 14/1/26.
+//
+
+import SwiftUI
+
+@main
+struct DrainGuardHCMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
